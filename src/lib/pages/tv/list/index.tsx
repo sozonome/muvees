7026c1f90315b4c99a903255bc5fd26f203dbf10
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-09-25 16:24:11 +0700

    feat: add pagination to tv shows list

--- src/lib/pages/tv/list/index.tsx
+++ src/lib/pages/tv/list/index.tsx
@@ -1,16 +1,34 @@
 import { Grid, Heading, Text } from "@chakra-ui/react";
+import * as React from "react";
 
+import type { PageNavButtonProps } from "lib/components/shared/list/PageNavButtons";
+import PageNavButtons from "lib/components/shared/list/PageNavButtons";
 import TvShowListContainer from "lib/components/tv/TvShowListContainer";
 import { useTVShowByList } from "lib/services/tmdb/tv/list";
 
 import type { TVShowListPageProps } from "./types";
 
-const TVShowList = ({ listType, data: fallbackData }: TVShowListPageProps) => {
-  const { data, isLoading } = useTVShowByList({ listType, fallbackData });
+const TVShowList = ({ listType }: TVShowListPageProps) => {
+  const [page, setPage] = React.useState<number>(1);
+  const { data, isLoading } = useTVShowByList({
+    listType,
+    params: { page },
+  });
 
-  if (!data) {
-    return null;
-  }
+  const handleClickNext = () =>
+    setPage((prevPage) =>
+      prevPage === data?.total_pages ? prevPage : prevPage + 1
+    );
+  const handleClickPrev = () =>
+    setPage((prevPage) => (prevPage === 0 ? prevPage : prevPage - 1));
+
+  const pageNavButtonProps: PageNavButtonProps = {
+    isLoading,
+    page,
+    totalPages: data?.total_pages ?? 0,
+    onClickNext: handleClickNext,
+    onClickPrev: handleClickPrev,
+  };
 
   return (
     <Grid gap={4} paddingX={8}>
@@ -18,7 +36,9 @@ const TVShowList = ({ listType, data: fallbackData }: TVShowListPageProps) => {
         <Heading>TV Shows</Heading>
         <Text textTransform="capitalize">{listType.replaceAll("_", " ")}</Text>
       </Grid>
-      <TvShowListContainer shows={data.results} isLoading={isLoading} />
+      <PageNavButtons {...pageNavButtonProps} />
+      <TvShowListContainer shows={data?.results} isLoading={isLoading} />
+      <PageNavButtons {...pageNavButtonProps} />
     </Grid>
   );
 };
