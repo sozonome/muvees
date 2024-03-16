5ec134cdd99c85643fd747c72cd7ce6a6954ba52
Agustinus Nathaniel <berserk.nate@gmail.com>
2024-03-16 11:01:05 +0700

    refactor: set person page to be client side

--- src/lib/pages/person/detail/index.tsx
+++ src/lib/pages/person/detail/index.tsx
@@ -4,88 +4,93 @@ import {
   Button,
   Grid,
   Heading,
+  Skeleton,
   Text,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 
 import { BionifiedParagraph } from 'lib/components/BionifiedParagraph';
 import PosterImage from 'lib/components/shared/PosterImage';
+import { usePersonDetail } from 'lib/services/tmdb/person/detail';
 import { countAge } from 'lib/utils/countAge';
 
-import type { PersonDetailPageProps } from './types';
-
-const PersonDetailPage = ({ data }: PersonDetailPageProps) => {
+const PersonDetailPage = () => {
   const router = useRouter();
+  const { id } = router.query;
+
+  const { data } = usePersonDetail(Number(id));
 
   return (
     <Grid marginX={8} gap={8}>
       <Button onClick={router.back}>back</Button>
 
-      <Box
-        display={{ base: 'grid', md: 'flex' }}
-        alignItems="start"
-        gap={{ base: 8, md: 16 }}
-      >
-        {data && (
-          <AspectRatio
-            minWidth={{ md: 300 }}
-            marginX={[8, '25%', 0]}
-            ratio={3.6 / 5}
-          >
-            <PosterImage
-              // style={{ filter: data.deathday && "grayscale(100%)" }}
-              src={data.profile_path}
-            />
-          </AspectRatio>
-        )}
-
-        <Box>
+      <Skeleton isLoaded={!!data}>
+        <Box
+          display={{ base: 'grid', md: 'flex' }}
+          alignItems="start"
+          gap={{ base: 8, md: 16 }}
+        >
           {data && (
-            <Heading
-              textAlign={['center', 'center', 'inherit']}
-              size="lg"
-              letterSpacing={2}
-              textTransform="uppercase"
-              fontWeight="extrabold"
-              marginX={[8, 8, 0]}
+            <AspectRatio
+              minWidth={{ md: 300 }}
+              marginX={[8, '25%', 0]}
+              ratio={3.6 / 5}
             >
-              {data.name}
-            </Heading>
+              <PosterImage
+                // style={{ filter: data.deathday && "grayscale(100%)" }}
+                src={data.profile_path}
+              />
+            </AspectRatio>
           )}
 
-          {data && (
-            <Grid gap={4}>
-              <Box
-                textTransform="uppercase"
+          <Box>
+            {data && (
+              <Heading
+                textAlign={['center', 'center', 'inherit']}
+                size="lg"
                 letterSpacing={2}
-                marginY={2}
-                fontSize="xs"
-                fontWeight="light"
+                textTransform="uppercase"
+                fontWeight="extrabold"
+                marginX={[8, 8, 0]}
               >
-                {data.deathday ? (
-                  <Text>
-                    {data.deathday} (
-                    {data.birthday
-                      ? countAge(data.birthday, data.deathday)
-                      : ''}{' '}
-                    years)
-                  </Text>
-                ) : (
-                  data.birthday && (
-                    <Text>Age : {countAge(data.birthday)} years</Text>
-                  )
-                )}
-              </Box>
-              <BionifiedParagraph
-                fontSize={{ base: 'sm', md: 'md' }}
-                lineHeight={1.75}
-              >
-                {data.biography}
-              </BionifiedParagraph>
-            </Grid>
-          )}
+                {data.name}
+              </Heading>
+            )}
+
+            {data && (
+              <Grid gap={4}>
+                <Box
+                  textTransform="uppercase"
+                  letterSpacing={2}
+                  marginY={2}
+                  fontSize="xs"
+                  fontWeight="light"
+                >
+                  {data.deathday ? (
+                    <Text>
+                      {data.deathday} (
+                      {data.birthday
+                        ? countAge(data.birthday, data.deathday)
+                        : ''}{' '}
+                      years)
+                    </Text>
+                  ) : (
+                    data.birthday && (
+                      <Text>Age : {countAge(data.birthday)} years</Text>
+                    )
+                  )}
+                </Box>
+                <BionifiedParagraph
+                  fontSize={{ base: 'sm', md: 'md' }}
+                  lineHeight={1.75}
+                >
+                  {data.biography}
+                </BionifiedParagraph>
+              </Grid>
+            )}
+          </Box>
         </Box>
-      </Box>
+      </Skeleton>
     </Grid>
   );
 };
--- src/pages/person/[id].ts
+++ src/pages/person/[id].ts
@@ -1,4 +1,3 @@
 import PersonDetailPage from 'lib/pages/person/detail';
 
-export { getStaticPaths, getStaticProps } from 'lib/pages/person/detail/loader';
 export default PersonDetailPage;
